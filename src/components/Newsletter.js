44d2fee0687c16513c22439f200ebe511b5bbd14
Vaibhav <khandelwalv971@gmail.com>
2023-07-30 12:27:58 +0530

    changenewsletter

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -5,7 +5,7 @@ export const Newsletter = () => {
   return (
       <Col lg={12}>
         <div className="newsletter-bx wow slideInUp">
-              <h3>Subscribe to our Newsletter & Never miss latest updates</h3>
+              <h3>Current Status: Preparing for placements!</h3>
         </div>
       </Col>
   )
